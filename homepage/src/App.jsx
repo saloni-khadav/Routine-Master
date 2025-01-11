555d885033537db5808dbeff0f4c3bee2ecae8db
saloni-khadav <salonikhadav@gmail.com>
2025-01-11 11:39:30 +0530

    new

--- homepage/src/App.jsx
+++ homepage/src/App.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
- import Progress1 from './component/progres/Progress1';
-
+import Progress1 from './component/progres/Progress1';
 import Progress from './component/progres/Progress';
-import Navcll from './component/navccal/navcll';
 import Hmpge from './component/Homepage/hmpge';
-import Task from './component/Homepage/hmpge';
-import Goal from './component/Goal';
+import Goal from './component/goall/Goal';
 import Welcomepage from './component/welcomepage/Welcomepage';
 
 
 
 function App() {
-         const domain="dev-r50wu5d3c4x5xbho.us.auth0.com";
-         const clientId= "8GmQbOZ6YKsSfbViw7bg05T0BRG7QRTA";
-  
+  const domain = "dev-r50wu5d3c4x5xbho.us.auth0.com";
+  const clientId = "8GmQbOZ6YKsSfbViw7bg05T0BRG7QRTA";
+
   return (
     <Auth0Provider
       domain={domain}
@@ -23,22 +20,22 @@ function App() {
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}>
-         
-    <Router>
-       <div className="App">
-        
-         
-        <Routes>
-          <Route path="/" element={<Welcomepage></Welcomepage>}></Route>
-          <Route path="/Hmpge" element={<Hmpge></Hmpge>}></Route>
-          <Route path="/Progress" element={<Progress></Progress>}></Route>
-          <Route path="/Progress1" element={<Progress1></Progress1>}></Route>
-          <Route path="/Goal" element={<Goal></Goal>}></Route>
-        
-        </Routes>
-         
-    </div>
-    </Router>
+
+      <Router>
+        <div className="App">
+
+
+          <Routes>
+            <Route path="/" element={<Welcomepage></Welcomepage>}></Route>
+            <Route path="/Hmpge" element={<Hmpge></Hmpge>}></Route>
+            <Route path="/Progress" element={<Progress></Progress>}></Route>
+            <Route path="/Progress1" element={<Progress1></Progress1>}></Route>
+            <Route path="/Goal" element={<Goal></Goal>}></Route>
+
+          </Routes>
+
+        </div>
+      </Router>
     </Auth0Provider>
   );
 }
